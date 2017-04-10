135b8148ad2be0e5035d1724905d72d058427215
Aleksandr Skoropletov <alex@skoropletov.ru>
2017-04-10 08:31:25 +0300

    0.7.0 - instadiff module - access token requesting - api requiesting

--- bin/instadiff.js
+++ bin/instadiff.js
@@ -1,20 +1,72 @@
 var config = require("../config");
-const InstagramNodeApi = require('instagram-node-api');
+var request = require("request");
 
-var instadiff = {};
+var instadiff = {
+    follows: {},
+    followed_by: {},
+    api_url: 'https://api.instagram.com/v1',
+    access_token: null,
+    user_id: null,
+    getUrl: function (method, callback) {
+        callback(
+            this.api_url
+            + "/users/self/"
+            + method
+            + "?access_token="
+            + this.access_token
+        );
+    },
+    getApiRequest: function (method, variable, callback) {
+        instadiff.getUrl(method, function (url) {
+            console.log(method);
+            console.log(url);
+            request(
+                url,
+                function (err, response, body) {
+                    if (err || response.statusCode !== 200) {
+                        return false;
+                    }
+                    console.log(method);
+                    console.log(JSON.parse(body));
+                    instadiff[variable] = JSON.parse(body);
+                    callback();
+                }
+            );
+        });
+    },
+    getAccessToken: function(query, callback) {
+        request.post({
+            url: "https://api.instagram.com/oauth/access_token",
+            form: {
+                client_id: config.instagram.clientId,
+                client_secret: config.instagram.clientSecret,
+                grant_type: 'authorization_code',
+                redirect_uri: config.url + config.instagram.redirect_uri + "?userid=" + query.login,
+                code: query.code
+            }
+        }, function(err, response, body) {
+            if (err || response.statusCode != 200) {
+                return {error: err};
+            }
+            instadiff.access_token = JSON.parse(body).access_token;
+            callback();
+        });
+    }
+};
 
 /**
- *
- * @param req
- * @returns {string}
- */
-instadiff.get = function (req) {
-    const instagramNodeApi = new InstagramNodeApi('');
-    diff = {
-        login: req.query.login
-    };
-
-    return diff;
+ * @param query
+ * @param callback
+  */
+instadiff.get = function (query, callback) {
+    this.user_id = query.login;
+    this.getAccessToken(query, function() {
+        instadiff.getApiRequest('follows', 'follows', function() {
+            instadiff.getApiRequest('followed-by', 'followed_by', function() {
+                callback(this.follows, this.followed_by);
+            });
+        });
+    });
 };
 
 module.exports = instadiff;
--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,63 @@ var instadiff = require("../bin/instadiff");
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render(
-        'index',
-        {
-            title: config.title,
-            description: config.description,
-            login: req.query.login || null,
-            diff: req.query.login ? instadiff.get(req) : null
-        }
-    );
+    if (req.query.code && req.query.login) {
+        instadiff.get(req.query, function () {
+            res.render(
+                'index',
+                {
+                    title: config.title,
+                    description: config.description,
+                    login: req.query.login,
+                    follows: instadiff.follows,
+                    followed_by: instadiff.followed_by
+                }
+            );
+        });
+    } else {
+        res.render(
+            'index',
+            {
+                title: config.title,
+                description: config.description,
+                login: null,
+                follows: null,
+                followed_by: null
+            }
+        );
+    }
 });
 
-router.get('/access', function (req, res, next) {
-    res.render(
-        'index',
-        {
-            title: config.title,
-            description: config.description,
-            login: req.query.login || null,
-            diff: req.query.login ? instadiff.get(req) : null
-        }
-    );
+router.get('/get_access', function (req, res, next) {
+    if (req.query.login) {
+        res.redirect(
+            "https://api.instagram.com/oauth/authorize/?client_id="
+            + config.instagram.clientId
+            + "&response_type=code"
+            + "&scope=follower_list"
+            + "&redirect_uri="
+            + encodeURIComponent(
+                config.url
+                + config.instagram.redirect_uri
+                + "?userid=" + req.query.login
+            )
+        );
+    } else {
+        res.redirect("/");
+    }
+});
+
+router.get('/get_token', function (req, res, next) {
+    if (req.query.code && req.query.userid) {
+        res.redirect(
+            "/?login="
+            + req.query.userid
+            + "&code="
+            + req.query.code
+        );
+    } else {
+        res.redirect("/");
+    }
 });
 
 module.exports = router;
